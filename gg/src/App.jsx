397cabc51f7a2ea044df180fa1cc7bac17bd73db
shokirjon <xhxjsusi@gmail.com>
2024-11-11 17:44:59 +0500

    navbar bot

---
+++ gg/src/App.jsx
@@ -0,0 +1,11 @@
+import { Typography, Card } from "@material-tailwind/react";
+import { NavbarDefault } from "./compontes/navabr";
+
+export default function App() {
+  return (
+    <div>
+      <NavbarDefault/>
+      <h1>Lorem ipsum dolor sit.</h1>
+    </div>
+  );
+}
