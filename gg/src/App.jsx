40d37000b9813b891cb52adc17b226c5ca0b2944
shokirjon <xhxjsusi@gmail.com>
2024-11-11 17:59:52 +0500

    car tayot

--- gg/src/App.jsx
+++ gg/src/App.jsx
@@ -1,11 +1,21 @@
 import { Typography, Card } from "@material-tailwind/react";
 import { NavbarDefault } from "./compontes/navabr";
+import { Card1 } from "./compontes/card";
+import { Card2 } from "./compontes/card2";
+import { Card3 } from "./compontes/card3";
 
 export default function App() {
   return (
     <div>
-      <NavbarDefault/>
-      <h1>Lorem ipsum dolor sit.</h1>
+     <div className="mb-10">
+     <NavbarDefault/>
+     </div>
+      <div className="flex justify-around ">
+      <Card1/>
+      <Card2/>
+      <Card3/>
+      </div>
+
     </div>
   );
 }
